e9107cfffd782da441da457864aeb35cf83ad342
doej96 <doej96@naver.com>
2021-03-02 15:42:00 +0900

    03

---
+++ js/03.product.js
@@ -0,0 +1,46 @@
+
+/*********** App ************/
+class App extends React.Component {
+	state = {
+		query: '가나다'
+	}
+	render() {
+		return (
+			<div className="container">
+				<TitleBar query={this.state.query} className="my-4"/>
+				<Search />
+			</div>
+		)
+	}
+}
+
+/*********** TitleBar ************/
+class TitleBar extends React.Component {
+	render() {
+		return (
+			<div className="jumbotron">
+				<h1>
+					React 쇼핑몰
+					<br />
+					<small>쇼핑몰 검색 | {this.props.query}</small>
+				</h1>
+			</div>
+		)
+	}
+}
+
+/**************** Search ******************/
+class Search extends React.Component {
+	handleChange = e => {
+		this.props.onChange(e.target.value)
+	}
+	render() {
+		return (
+			<form className="form-search">
+				<input className="form-control" type="text" onChange={this.handleChange} autoFocus />
+			</form>
+		)
+	}
+}
+
+ReactDOM.render(<App />, document.getElementById('app'))
